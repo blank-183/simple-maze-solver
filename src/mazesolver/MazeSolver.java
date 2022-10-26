0a066a351e484192f748560d6112ec68aeeb4953
Ivee Jintalan <iveejintalan17@gmail.com>
2022-10-27 02:40:45 +0800

    new project

---
+++ src/mazesolver/MazeSolver.java
@@ -0,0 +1,10 @@
+package mazesolver;
+
+public class MazeSolver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author iveej
+ *
+ */
+module SimpleMazeSolver {
+}
