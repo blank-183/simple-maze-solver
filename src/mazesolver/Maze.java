39bb81e99a7f78d63f01027df0d832d52f221282
Ivee Jintalan <iveejintalan17@gmail.com>
2022-10-27 04:15:35 +0800

    create new Maze class

---
+++ src/mazesolver/Maze.java
@@ -0,0 +1,70 @@
+package mazesolver;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Maze {
+	int[][] matrix;
+	int colSize;
+	int rowSize;
+	
+	Maze(String file) {
+		File tf = new File(file);
+		Scanner sc = null;
+		
+		int lineCount = 0;
+		
+		// checks if file exists
+		try {
+			sc = new Scanner(tf);
+		} catch (FileNotFoundException e) {
+			System.out.println("The file " + tf.toString() + " is not available.");
+			System.exit(1);
+		}
+		
+		String[] sizeStr = sc.nextLine().split(" ");
+		
+		// checks if the size is correct
+		if(sizeStr.length == 2) {
+			colSize = Integer.parseInt(sizeStr[0]);
+			rowSize = Integer.parseInt(sizeStr[1]);
+			
+		} else {
+			System.out.println("The maze size provided in the text file is incorrect!");
+			sc.close();
+			System.exit(1);
+		}
+		
+		matrix = new int[rowSize][colSize];
+		
+		while(sc.hasNextLine()) {
+			if(lineCount > rowSize) {
+				System.out.println("The line count does not match the given row size.");
+				sc.close();
+				System.exit(1);
+			}
+			String[] rowStr = sc.nextLine().split(" ");
+			if(rowStr.length != colSize) {
+				System.out.println("The column size of the current row does not match the given column size.");
+				sc.close();
+				System.exit(1);
+			}
+			
+			for(int i = 0; i < colSize; i++) {
+				matrix[lineCount][i] = Integer.parseInt(rowStr[i]);
+			}
+			
+			lineCount++;
+		}
+		
+		if (lineCount != rowSize) {
+			System.out.println("Hello");
+			System.out.println("The line count does not match the given row size.");
+			sc.close();
+			System.exit(1);
+		}
+		
+		sc.close();
+	}
+}
--- src/mazesolver/MazeSolver.java
+++ src/mazesolver/MazeSolver.java
@@ -1,10 +1,22 @@
 package mazesolver;
 
+/**
+ * 
+ * @author iveej
+ *
+ */
+
 public class MazeSolver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/**
+		 * 0 = wall
+		 * 1 = path
+		 * 2 = destination
+		 */
+		
+		Maze maze = new Maze("maze.txt");
+		
 	}
 
 }
